875d56b8aa6da2469114bea862e76c7b3a908427
Sascha <Tohwa.gaming@gmail.com>
2023-05-30 16:24:07 +0200

    added movement and jump script prep work for shrrom interaction

--- Studyweek_30_5_2023_Jump'n'run/Assets/Scripts/Interaction.cs
+++ Studyweek_30_5_2023_Jump'n'run/Assets/Scripts/Interaction.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class movement : MonoBehaviour
+public class Interaction : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
