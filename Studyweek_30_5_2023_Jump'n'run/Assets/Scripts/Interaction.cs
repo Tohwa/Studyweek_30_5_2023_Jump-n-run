04e55e99e5811e16776d8d1383809ccf013e377f
Sascha <Tohwa.gaming@gmail.com>
2023-06-01 11:51:40 +0200

    added menu scenes

--- Studyweek_30_5_2023_Jump'n'run/Assets/Scripts/Interaction.cs
+++ Studyweek_30_5_2023_Jump'n'run/Assets/Scripts/Interaction.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.SceneManagement;
 
 public class Interaction : MonoBehaviour
 {
@@ -8,12 +10,14 @@ public class Interaction : MonoBehaviour
     [Header("Booleans")]
     public bool shroomEaten;
     public bool keyTaken;
+    public bool doorReached;
     #endregion
 
     private void Start()
     {
         shroomEaten = false;
         keyTaken = false;
+        doorReached = false;
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
@@ -26,6 +30,31 @@ public class Interaction : MonoBehaviour
         if (collision.gameObject.CompareTag("Shroom"))
         {
             shroomEaten = true;
+        }
+
+        if (collision.gameObject.CompareTag("Water"))
+        {
+            SceneManager.LoadScene("Game Over");
+        }
+        else if (collision.gameObject.CompareTag(""))
+        {
+            SceneManager.LoadScene("Game Over");
+        }
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Shroom"))
+        {
+            doorReached = true;
+        }
+    }
+
+    public void EnterHome(InputAction.CallbackContext ctx)
+    {
+        if(ctx.started && doorReached)
+        {
+            SceneManager.LoadScene("Victory");
         }
     }
 }
